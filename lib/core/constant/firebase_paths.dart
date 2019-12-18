a5b63c244cdd2fc4dbdcda0bc129d212a06afc12
mayankgenie <mayank.gupta.genie@gmail.com>
2019-12-19 03:24:39 +0530

    add models etc

--- lib/core/constant/firebase_paths.dart
+++ lib/core/constant/firebase_paths.dart
@@ -4,4 +4,22 @@ class FirebasePaths {
 
   static const base_url = '';
   static const recommended_services = 'recommended_services';
+  static const services = 'services';
+  static const users = 'users';
+  static const stories = 'stories';
+  static const categories = 'categories';
+  static const adhoc_requests = 'adhoc_requests';
+  static const agents = 'agents';
+  static const providers = 'providers';
+  static const reviews = 'reviews';
+  static const carts = 'carts';
+  static const orders = 'orders';
+  static const charts = 'charts';
+  static const tasks = 'tasks';
+  static const earnings = 'earnings';
+  static const merchants = 'merchants';
+  static const teams = 'teams';
+
+
+
 }
