659496973c5e4402255a4d09aa91f7b346a352ec
mayank <mayank.gupta.genie@gmail.com>
2020-01-03 06:48:41 +0530

    Update adhoc_requests_repository.dart

--- lib/core/repositories/adhoc_requests_repository/adhoc_requests_repository.dart
+++ lib/core/repositories/adhoc_requests_repository/adhoc_requests_repository.dart
@@ -1,5 +1,5 @@
 import 'package:servplatform/core/models/adhoc_request/adhoc_request.dart';
 
 abstract class AdhocRequestsRepository {
-  Future<List<AdhocRequest>> fetchAdhoc_requests();
+  Future<List<AdhocRequest>> fetchAdhocRequests();
 }
