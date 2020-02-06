5ab0ade9a9d971b600a8c42dac41a6347a9e0f39
mayankgenie <mayank.gupta.genie@gmail.com>
2020-02-05 22:10:56 -0800

    fix android errors

--- lib/core/repositories/posts_repository/posts_repository.dart
+++
@@ -1,5 +0,0 @@
-import 'package:servplatform/core/models/post/post.dart';
-
-abstract class PostsRepository {
-  Future<List<Post>> fetchPosts();
-}
--- lib/core/services/local_storage/local_storage_service.dart
+++ lib/core/services/local_storage/local_storage_service.dart
@@ -1,8 +1,6 @@
 import 'package:hive/hive.dart';
-import 'package:servplatform/core/models/post/post_h.dart';
 
 abstract class LocalStorageService {
   Future<void> init();
 
-  Box<PostH> get postsBox;
 }
