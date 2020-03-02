144820236fe1f996dab914703082974d4860a7ee
Kshitij <kshitij@Kshitijs-Air.Dlink>
2020-03-02 21:29:18 +0530

    search ui

--- lib/ui/widgets/search_field.dart
+++ lib/ui/widgets/search_field.dart
@@ -1,61 +1,104 @@
 import 'dart:core';
 
 import 'package:flutter/material.dart';
-import 'package:servplatform/core/constant/view_routes.dart';
-import 'package:servplatform/core/models/service/service.dart';
-import 'package:servplatform/ui/widgets/add_button.dart';
-//import 'package:real_rich_text/real_rich_text.dart';
 
-class SearchField extends StatelessWidget {
+class SearchField extends StatefulWidget {
   //final TextEditingController searchControl;
 
-  SearchField();
+  @override
+  _SearchFieldState createState() => _SearchFieldState();
+}
 
+class _SearchFieldState extends State<SearchField> {
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
 
-    return Container(
-      padding: EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(
-          Radius.circular(8.0),
-        ),
-      ),
-      child: TextField(
-        style: textTheme.caption,
-        decoration: InputDecoration(
-          contentPadding: EdgeInsets.all(8.0),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8.0),
-            borderSide: BorderSide(
-              color: Colors.grey[400],
-            ),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.grey[400],
-            ),
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          hintText: "",
-          prefixIcon: Icon(
-            Icons.search,
-            color: Colors.grey[400],
-          ),
-          suffixIcon: Icon(
-            Icons.filter_list,
-            color: Colors.grey[400],
-          ),
-          hintStyle: TextStyle(
-            fontSize: 15.0,
-            color: Colors.grey[400],
+    return GestureDetector(
+      onTap: () {
+        showSearch(context: context, delegate: _searchBarDelegate());
+      },
+      child: Container(
+        padding: EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(8.0),
           ),
         ),
-        maxLines: 1,
-        //controller: searchControl,
+        child: IgnorePointer(
+          child: TextField(
+            style: textTheme.caption,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(8.0),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+                borderSide: BorderSide(
+                  color: Colors.grey[400],
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.grey[400],
+                ),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              hintText: "",
+              prefixIcon: Icon(
+                Icons.search,
+                color: Colors.grey[400],
+              ),
+              suffixIcon: Icon(
+                Icons.filter_list,
+                color: Colors.grey[400],
+              ),
+              hintStyle: TextStyle(
+                fontSize: 15.0,
+                color: Colors.grey[400],
+              ),
+            ),
+            maxLines: 1,
+            //controller: searchControl,
+          ),
+        ),
       ),
     );
+  }
+}
+
+class _searchBarDelegate extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = '';
+          })
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    return IconButton(
+        icon: AnimatedIcon(
+            icon: AnimatedIcons.menu_arrow, progress: transitionAnimation),
+        onPressed: () {
+          close(context, null);
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    return null;
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    return Center(child: CircularProgressIndicator());
   }
 }
