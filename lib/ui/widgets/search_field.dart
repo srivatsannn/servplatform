e01aa93d85f031a7c1ee71eadc35658d4ab5ee10
Jacob2071 <jacob.bosco3075@gmail.com>
2020-03-09 20:07:20 +0530

    Added proper handling of search input

--- lib/ui/widgets/search_field.dart
+++ lib/ui/widgets/search_field.dart
@@ -4,8 +4,6 @@ import 'package:algolia/algolia.dart';
 import 'package:flutter/material.dart';
 
 class SearchField extends StatefulWidget {
-  //final TextEditingController searchControl;
-
   @override
   _SearchFieldState createState() => _SearchFieldState();
 }
@@ -15,11 +13,47 @@ class Application {
       applicationId: "4G14T29E29", apiKey: "fee95dde1d83700139a669fa893");
 }
 
+Algolia algolia = Application.algolia;
+
+class NewData {
+  String serviceProvider;
+
+  NewData({this.serviceProvider, this.serviceType, this.price});
+
+  String serviceType;
+  int price;
+}
+
+List<NewData> newData = [
+  NewData(serviceProvider: 'Urban Clap', serviceType: 'Cleaning1', price: 150),
+  NewData(serviceProvider: 'Urban Clap', serviceType: 'Cleaning2', price: 150),
+  NewData(serviceProvider: 'Urban Clap', serviceType: 'Cleaning3', price: 150),
+  NewData(serviceProvider: 'Urban Clap', serviceType: 'Haircut1', price: 100),
+  NewData(serviceProvider: 'Urban Clap', serviceType: 'Haircut2', price: 100),
+  NewData(serviceProvider: 'Urban Clap', serviceType: 'Haircut3', price: 100),
+];
+
 class _SearchFieldState extends State<SearchField> {
+  @override
+  void initState() /*async*/ {
+    // TODO: implement initState
+    super.initState();
+//    for (int i = 0; i < newData.length; i++) {
+//      Map<String, dynamic> adddata = {/**/
+//        'serviceProvider': newData[i].serviceProvider,
+//        'serviceType': newData[i].serviceType,
+//        'price': newData[i].price,
+//      };
+//
+//      AlgoliaTask taskAdded =
+//          await algolia.instance.index('serv').addObject(adddata);
+//      print(taskAdded.data);
+//    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-
     return GestureDetector(
       onTap: () {
         showSearch(context: context, delegate: _searchBarDelegate());
@@ -73,8 +107,6 @@ class _SearchFieldState extends State<SearchField> {
 }
 
 class _searchBarDelegate extends SearchDelegate {
-  Algolia algolia = Application.algolia;
-
   @override
   List<Widget> buildActions(BuildContext context) {
     // TODO: implement buildActions
@@ -100,16 +132,50 @@ class _searchBarDelegate extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    // TODO: implement buildResults
-    AlgoliaQuery newQuery = algolia.instance.index("Providers").search(query);
-    print(newQuery.setFacetFilter('status:published'));
-    print(newQuery.setFacetFilter('isDelete:false'));
+    AlgoliaQuery searchQuery =
+        algolia.instance.index("Providers").search(query);
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text(query),
-        Text(newQuery.setFacetFilter('status:published').toString()),
-        Text(newQuery.setFacetFilter('isDelete:false').toString()),
+        FutureBuilder(
+          future: searchQuery.getObjects(),
+          builder: (BuildContext context,
+              AsyncSnapshot<AlgoliaQuerySnapshot> snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text("Loading...."),
+                );
+                break;
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text("Error"),
+                  );
+                } else {
+                  return ListView.separated(
+                    itemBuilder: (context, index) {
+                      final AlgoliaObjectSnapshot result =
+                          snapshot.data.hits[index];
+                      return ListTile(
+                        title: Text(
+                          result.data['serviceProvider'],
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 18,
+                          ),
+                        ),
+                        subtitle: Text(result.data['country']),
+                      );
+                    },
+                    separatorBuilder: (context, index) => Divider(
+                      color: Colors.grey,
+                    ),
+                    itemCount: snapshot.data.hits.length,
+                  );
+                }
+            }
+          },
+        ),
       ],
     );
   }
